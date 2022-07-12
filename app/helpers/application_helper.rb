e2f25e80ad847737f19a74ca5c7d9e5a783572cb
Dipesh Gupta <dipesh.gupta@w3villa.com>
2022-07-12 18:35:10 +0530

    Added validations for images and blog title and body

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+    def thumbnail(image)
+        image.variant(resize: '500x500').processed
+    end
+
+    def find_time(t)
+        a = t.in_time_zone(TZInfo::Timezone.get('Asia/Kolkata'))
+        a.strftime("at %I : %M %p")
+    end
+     
+    def find_date(d)
+        d.strftime("%d %b, %y") 
+    end
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,11 @@
 class Blog < ApplicationRecord
+
+    validates :title, presence: true, uniqueness: { case_sensitive: true }, length: {maximum: 50, minimum:5}
+    validates :body, presence: true, uniqueness: { case_sensitive:  true }, length: { minimum: 10 }
+    validate :correct_image_type
+    
+    
+
     belongs_to :user
     belongs_to :category
     has_many_attached :images
@@ -6,6 +13,17 @@ class Blog < ApplicationRecord
     has_many :blog_tags
     has_many :tags, through: :blog_tags
 
+    def correct_image_type
+        if images.attached? == false
+            errors.add(:images, "are missing")
+        end
+
+        images.each do |image|
+            if !image.content_type.in?(%w(image/jpeg image/png))
+                errors.add(:image,"must be a JPG/JPEG/PNG")
+            end
+        end
+    end
 #   accepts_nested_attributes_for :tag, allow_destroy: true
 
     def tags_attributes=(tags_attributes)
